d2cc9a5c786a283a3bac233a9098bfe6c65a110c
vijay more <more.vijay@gmail.com>
2021-09-05 21:59:07 +0530

    feature: local cache for network uris

--- appiocache/appiocache.cs
+++ appiocache/appiocache.cs
@@ -2,6 +2,7 @@
 namespace appiocache
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Net.Http;
     using System.Security.Cryptography;
@@ -14,20 +15,51 @@ namespace appiocache
         public appiocache(byte[] vs)
         {
             dataStream = new MemoryStream(vs);
+            LastUpdated = DateTime.UtcNow;
         }
-        public appiocache(Uri uri, CancellationToken? cancelFetch=null)
+        public static async Task<appiocache> fromUri(Uri uri, CancellationToken? cancellationToken = null, bool BypassCache = false)
         {
-            var byteContent = (cancelFetch.HasValue) ? client.GetByteArrayAsync(uri, cancelFetch.Value).Result : client.GetByteArrayAsync(uri).Result;
-            dataStream = new MemoryStream(byteContent);
-        }
-        public static async Task<appiocache> fromUri(Uri uri, CancellationToken? cancellationToken = null)
-        {
-            var byteContent = (cancellationToken.HasValue) ? await client.GetByteArrayAsync(uri, cancellationToken.Value) : await client.GetByteArrayAsync(uri);
+            if (!BypassCache && IsCacheAvailable(uri))
+                return theCache[uri];
+
+            byte[] byteContent;
+            try
+            {
+                byteContent = (cancellationToken.HasValue) ? await client.GetByteArrayAsync(uri, cancellationToken.Value) : await client.GetByteArrayAsync(uri);
+            }
+            catch (TaskCanceledException)
+            {
+                return null;
+            }
+            if (!BypassCache)
+                updateCache(uri, ref byteContent);
+
             return new appiocache(byteContent);
         }
 
+        private static Dictionary<Uri, appiocache> theCache = new Dictionary<Uri, appiocache>();
+        public static void clearCache()
+        {
+            theCache.Clear();
+        }
+        public static bool IsCacheAvailable(Uri uri)
+        {
+            return theCache.ContainsKey(uri);
+        }
+        public static appiocache getfromCache(Uri staticUri)
+        {
+            if (theCache.ContainsKey(staticUri))
+                return theCache[staticUri];
+            return null;
+        }
+        private static void updateCache(Uri uri, ref byte[] byteContent)
+        {
+            theCache[uri] = new appiocache(byteContent);
+        }
+        public DateTime LastUpdated { get; private set; } //latest cache update time
+
         private static readonly HttpClient client = new HttpClient(); // singleton to support recommended reuse
-        public string Checksum { get {return this.findHash();} } // hash (sha256) helps uniquely & easily identify the data bytes
+        public string Checksum { get { return this.findHash(); } } // hash (sha256) helps uniquely & easily identify the data bytes
         private MemoryStream dataStream; // data is viewed as in-memory stream, feeded by network, persisted to disk, etc
 
         private string findHash()
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -8,9 +8,13 @@ namespace tests
     using System.Collections.Generic;
     using System.Net.Http;
     using System.Threading.Tasks;
+    using System.Threading;
 
     public class Tests
     {
+        static readonly Uri staticUri = new System.Uri(@"https://raw.githubusercontent.com/vijiboy/declarative-camera/master/images/toolbutton.png");
+        static readonly CancellationToken cancelledAlready = new CancellationToken(canceled: true);
+        static readonly CancellationToken cancelNever = new CancellationToken(canceled: false);
         static HttpClient tempclient;
         [OneTimeSetUp]
         public static void Setup()
@@ -27,8 +31,10 @@ namespace tests
         [Test]
         public void dataCache_FromBytes_ChecksumVerified()
         {
-            Assert.IsTrue(string.Equals(new appiocache(new byte[] {0xff,0xff}).Checksum, "CA2FD00FA001190744C15C317643AB092E7048CE086A243E2BE9437C898DE1BB", System.StringComparison.OrdinalIgnoreCase));
-            Assert.IsTrue(string.Equals(new appiocache(System.Text.Encoding.UTF8.GetBytes("Hello World!")).Checksum, "7F83B1657FF1FC53B92DC18148A1D65DFC2D4B1FA3D677284ADDD200126D9069", System.StringComparison.OrdinalIgnoreCase));
+            Assert.IsTrue(string.Equals(new appiocache(new byte[] {0xff,0xff}).Checksum, 
+            "CA2FD00FA001190744C15C317643AB092E7048CE086A243E2BE9437C898DE1BB", System.StringComparison.OrdinalIgnoreCase));
+            Assert.IsTrue(string.Equals(new appiocache(System.Text.Encoding.UTF8.GetBytes("Hello World!")).Checksum, 
+            "7F83B1657FF1FC53B92DC18148A1D65DFC2D4B1FA3D677284ADDD200126D9069", System.StringComparison.OrdinalIgnoreCase));
         }
 
         [Test]
@@ -42,16 +48,16 @@ namespace tests
         [Test]
         public void dataCache_From_NetworkResource_Synchronous()
         {
-            var fromStaticUri = new appiocache(new System.Uri(@"https://raw.githubusercontent.com/vijiboy/declarative-camera/master/images/toolbutton.png"));
-            var fromDownloadedBytes = new appiocache(tempclient.GetByteArrayAsync("https://raw.githubusercontent.com/vijiboy/declarative-camera/master/images/toolbutton.png").Result);
+            var fromStaticUri = appiocache.fromUri(staticUri).Result;
+            var fromDownloadedBytes = new appiocache(tempclient.GetByteArrayAsync(staticUri).Result);
             Assert.AreEqual(fromDownloadedBytes.Checksum, fromStaticUri.Checksum);
         }
 
         [Test]
         public void dataCache_From_NetworkResource_Asynchronous()
         {
-            var asyncDataRequest = appiocache.fromUri(new System.Uri(@"https://raw.githubusercontent.com/vijiboy/declarative-camera/master/images/toolbutton.png"));
-            var syncData= new appiocache(new System.Uri(@"https://raw.githubusercontent.com/vijiboy/declarative-camera/master/images/toolbutton.png"));
+            var asyncDataRequest = appiocache.fromUri(staticUri);
+            var syncData= appiocache.fromUri(staticUri).Result;
             asyncDataRequest.Wait();
             Assert.AreEqual(asyncDataRequest.Result.Checksum, syncData.Checksum);
         }
@@ -59,20 +65,19 @@ namespace tests
         [Test]
         public void dataCache_AsynchronousIO_Is_Faster()
         {
-            var sameUri = new System.Uri(@"https://raw.githubusercontent.com/vijiboy/declarative-camera/master/images/toolbutton.png");
             List<Task<appiocache>> asyncTasks = new List<Task<appiocache>>();
             List<appiocache> syncResults = new List<appiocache>();
-            const int TotalCalls = 200;
+            const int TotalCalls = 100;
             var AsyncCallsTime = System.Diagnostics.Stopwatch.StartNew(); 
             for (int i = 0; i < TotalCalls; i++)
-                asyncTasks.Add(appiocache.fromUri(sameUri));
+                asyncTasks.Add(appiocache.fromUri(staticUri,cancelNever,BypassCache:true));
             for (int i = 0; i < TotalCalls; i++)
                 asyncTasks[i].Wait();
             AsyncCallsTime.Stop();
 
             var SyncCallsTime = System.Diagnostics.Stopwatch.StartNew(); 
             for (int i = 0; i < TotalCalls; i++)
-                syncResults.Add(new appiocache(sameUri));
+                syncResults.Add(appiocache.fromUri(staticUri,cancelNever,BypassCache:true).Result);
             SyncCallsTime.Stop();
 
             for (int i = 0; i < TotalCalls; i++)
@@ -81,5 +86,61 @@ namespace tests
             Console.WriteLine($"Total Calls {TotalCalls}, AsyncCalls Time: {AsyncCallsTime.Elapsed.TotalSeconds.ToString()}, SyncCalls Time: {SyncCallsTime.Elapsed.TotalSeconds.ToString()}");
             Assert.True(TimeSpan.Compare(AsyncCallsTime.Elapsed, SyncCallsTime.Elapsed) == -1 );
         }
+
+        [Test]
+        public void NetworkFetch_CanBeBlocked_UsingCancellationToken()
+        {
+            appiocache.clearCache();
+            Assert.AreEqual(null, appiocache.fromUri(staticUri, cancelledAlready,BypassCache:true).Result);
+        }
+
+        [Test]
+        public void Cache_NetworkUri_Delivered_From_Cache_IfAvailable()
+        {
+            appiocache.clearCache();
+            Assert.IsNull(appiocache.fromUri(staticUri, new CancellationToken(canceled: true)).Result, "cancel failed: Network blocking must succeed for cache test");
+            var NetworkResource = appiocache.fromUri(staticUri).Result;
+            Assert.AreEqual(NetworkResource.Checksum, appiocache.getfromCache(staticUri).Checksum, "cache copy corrupt: mismatched with network copy");
+            Assert.AreEqual(NetworkResource.Checksum, appiocache.fromUri(staticUri, new CancellationToken(canceled: true), BypassCache:false).Result.Checksum, "unexpected cache-miss"); 
+        }
+
+
+        [Test]
+        public void Cache_ForceRefresh_Fetches_NetworkUri()
+        {
+            appiocache.clearCache();
+            var NetworkResource = appiocache.fromUri(staticUri).Result;
+            Assert.IsTrue(appiocache.IsCacheAvailable(staticUri));
+            var NetworkResourceRepeat = appiocache.fromUri(staticUri, cancelNever, BypassCache:true).Result;
+            Assert.True(NetworkResourceRepeat.LastUpdated > NetworkResource.LastUpdated, "expected Cache update to be recent(greater) than first update");
+        }
+
+        [Test]
+        public void Cache_Is_Even_Faster_Than_AsynchronousIO()
+        {
+            List<Task<appiocache>> asyncTasks = new List<Task<appiocache>>();
+            List<appiocache> syncResults = new List<appiocache>();
+            const int TotalCalls = 100;
+            var AsyncCallsTime = System.Diagnostics.Stopwatch.StartNew(); 
+            for (int i = 0; i < TotalCalls; i++)
+                asyncTasks.Add(appiocache.fromUri(staticUri,cancelNever,BypassCache:true));
+            for (int i = 0; i < TotalCalls; i++)
+                asyncTasks[i].Wait();
+            AsyncCallsTime.Stop();
+
+            var SyncCallsTime = System.Diagnostics.Stopwatch.StartNew(); 
+            for (int i = 0; i < TotalCalls; i++)
+                syncResults.Add(appiocache.fromUri(staticUri,cancelNever,BypassCache:false).Result);
+            SyncCallsTime.Stop();
+
+            for (int i = 0; i < TotalCalls; i++)
+                Assert.AreEqual(asyncTasks[i].Result.Checksum, syncResults[i].Checksum);
+
+            Console.WriteLine($"Total Calls {TotalCalls}, AsyncCalls Time: {AsyncCallsTime.Elapsed.TotalSeconds.ToString()}, SyncCallsWithCache Time: {SyncCallsTime.Elapsed.TotalSeconds.ToString()}");
+            Assert.True(TimeSpan.Compare(AsyncCallsTime.Elapsed, SyncCallsTime.Elapsed) == 1 );
+        }
+
+
     }
+
 }
